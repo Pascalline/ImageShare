18706afc5d22d9e2b21f8ff88d6661ea27cbbe46
Pascal <105133590+Pascalline@users.noreply.github.com>
2022-06-06 01:15:04 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import * as Sharing from 'expo-sharing';
+
+import logo from './assets/logo.png';
+// import uploadToAnonymousFilesAsync from 'anonymous-files'; 
+
+export default function App() {
+  const [selectedImage, setSelectedImage] = React.useState(null);
+
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false){
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    if (Platform.OS === 'web') {
+      let remoteUri = await uploadToAnonymousFilesAsync(pickerResult.uri);
+      setSelectedImage({ localUri: pickerResult.uri, remoteUri });
+    } else {
+      setSelectedImage({ localUri: pickerResult.uri, remoteUri: null });
+    };
+
+  };
+
+  let openShareDialogAsync = async () => {
+    if (!(await Sharing.isAvailableAsync())) {
+      alert(`The image is available for sharing at: ${selectedImage.remoteUri} `);
+      return;
+    }
+
+    Sharing.shareAsync(selectedImage.remoteUri || selectedImage.localUri);
+  }
+
+  if(selectedImage !== null){
+  return (
+    <View style={styles.container}>
+      <Image
+        source ={{ uri: selectedImage.localUri }}
+        style={styles.thumbnail}
+      />
+      <TouchableOpacity
+        onPress={openShareDialogAsync}
+        style={styles.button} >
+        <Text style={styles.buttonText} >Share this photo</Text>
+      </TouchableOpacity>
+    </View>
+  );}
+
+  return (
+    <View style={styles.container}>
+        <Image source={logo} style={styles.logo} />
+        <Text style={styles.instructions}>To share a photo with a friend, just press the button below!</Text>
+
+        <TouchableOpacity
+          onPress={openImagePickerAsync}
+          style= {styles.button}>
+            <Text style={styles.buttonText} >Pick a photo</Text>
+        </TouchableOpacity>
+            </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    width: "90%",
+    height: "50%",
+    marginBottom: 30,
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
+    marginBottom: 25,
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 15,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
+  },
+  thumbnail: {
+    width: "70%",
+    height: "70%",
+    resizeMode: "contain",
+    borderWidth: 2,
+    borderColor: "#888",
+    marginBottom: 25,
+  }
+});
